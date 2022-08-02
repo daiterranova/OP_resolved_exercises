e72b20a23e423d7357edc7c669d7a853342e77f7
daiterranova <daiana.terranova@gmail.com>
2022-08-02 14:29:31 -0300

    added Java folder with Java project and create new folder containing JS files

--- unit_one/unitOneJS/control_statements/switch.js
+++ unit_one/unitOneJS/control_statements/switch.js
@@ -1,4 +1,7 @@
 /*Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.*/
+
+/* for the switch loop, create the variable "estacion" and different cases for the four seasons of the year. Depending on the current value's variable, print on the screen a message that informs which is the season. Also, set the default case when the value's variable it's not a season. */
+
 let estacion = "papafrita";
 switch (estacion) {
   case "invierno":
---
+++ unit_one/unitOneJava/Java/encapsulacion/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args){
+        Person person = new Person();
+        person.setAge(30);
+        person.setName("Marcos");
+        person.setPhoneNumber(11569409);
+        person.getName();
+        person.getPhoneNumber();
+        person.getAge();
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Phone Number: " + person.getPhoneNumber());
+
+    }
+}
+
+class Person{
+    private int age;
+    private String name;
+    private int phoneNumber;
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setPhoneNumber(int phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    public int getPhoneNumber(){
+        return phoneNumber;
+    }
+
+}
