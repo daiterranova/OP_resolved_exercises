99ab250e407081b960db288ed5ee35991184d665
daiterranova <daiana.terranova@gmail.com>
2022-10-17 11:22:20 -0300

    add changes

--- Javascript/2.exercise/variables.js
+++ Javascript/2.exercise/variables.js
@@ -1,10 +1,12 @@
-let name = "Daiana";
-let age = 29;
-let developer = true;
-let birthday = new Date(1993, 3, 53);
-let favoriteBook = {
-  title: "So good they cant ignore you",
-  author: "Cal Newport",
-  published: "2012",
-  url: "https://www.amazon.com/Good-They-Cant-Ignore-You/dp/1455509124",
-};
+const list = [
+  "Daiana",
+  29,
+  true,
+  new Date(1987, 10, 20),
+  (favoriteBook = {
+    title: "So good they cant ignore you",
+    author: "Cal Newport",
+    published: "2012",
+    url: "https://www.amazon.com/Good-They-Cant-Ignore-You/dp/1455509124",
+  }),
+];
