66fa2e59be1627fa2147b59939dedb21675e969f
daiterranova <daiana.terranova@gmail.com>
2022-10-17 11:16:58 -0300

    add solution exercise 2

---
+++ Javascript/2.exercise/variables.js
@@ -0,0 +1,10 @@
+let name = "Daiana";
+let age = 29;
+let developer = true;
+let birthday = new Date(1993, 3, 53);
+let favoriteBook = {
+  title: "So good they cant ignore you",
+  author: "Cal Newport",
+  published: "2012",
+  url: "https://www.amazon.com/Good-They-Cant-Ignore-You/dp/1455509124",
+};
