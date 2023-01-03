8f89ee83afbf7667cd875f6984ac227ca6d1e025
daiterranova <daiana.terranova@gmail.com>
2023-01-03 19:24:26 -0300

    exercise eleven solved

---
+++ python_OB/11.exercise/alumns.sql
@@ -0,0 +1,6 @@
+/* CREATE TABLE students(id INT NOT NULL PRIMARY KEY, name TEXT NOT NULL, lastname TEXT NOT NULL); */
+
+/* INSERT INTO students(id,name,lastname) 
+VALUES(8,"Alex","Santander"); */
+
+SELECT name FROM students;
